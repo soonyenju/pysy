62fdaf2be91ba920d4832ef92daf4bbfc4a209c9
Songyan Zhu <zhusy93@gmail.com>
2020-04-28 15:00:49 +0100

    Update publish.py

--- publish.py
+++ publish.py
@@ -1,9 +1,23 @@
 import os
+import shutil
 
 if __name__ == "__main__":
-    # build
+    # publish_folders = [
+    #     "build",
+    #     "dist",
+    #     "pysy.egg-info"
+    # ]
+    # print("checking dirs...")
+    cur_dirs = os.listdir()
+    # build   
     os.system("python setup.py sdist bdist_wheel")
     print("package is built...")
     # push
     os.system("twine upload --repository-url https://upload.pypi.org/legacy/ dist/*")
     print("package is publised...")
+    print("clearing up...")
+    new_dirs = [p for p in os.listdir() if p not in cur_dirs]
+    for p in new_dirs:
+        # print(p)
+        shutil.rmtree(p)
+    print("all done.")
